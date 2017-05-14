b89f3efc67d81e60e47161a51e505cc636f77182
Lily Blue <unityrate@gmail.com>
2017-05-14 10:37:57 -0500

    Initial Commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+
+	public Vector3 direction = Vector3.forward;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += transform.rotation * (direction.normalized * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/MoveLeftAndRightL.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftAndRightL : MonoBehaviour 
+{
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//This moves our object left and right via player input.
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+	}
+}
