240b0eb367d8806e03a99392fbbd65a0363bc9a2
Lily Blue <unityrate@gmail.com>
2017-05-14 11:32:02 -0500

    Enemies Asplode

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GibOnTrigger : MonoBehaviour 
+{
+
+	public GameObject gib;
+
+	void OnTriggerEnter()
+	{
+		Instantiate(gib,transform.position,Quaternion.identity);
+		Destroy (gameObject);
+
+	}
+
+
+}
